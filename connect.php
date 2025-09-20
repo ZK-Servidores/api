7d181d264c57d7bd8d49bda933ccbd3ca5047fd6
crashzk <crashzk@zkservidores.com>
2025-09-20 17:38:24 -0300

    Commit initial

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_GET['ip'])) {
+    [$domainWithoutPort, $port] = explode(':', trim($_GET['ip'])) + [null, null];
+    $ip = gethostbyname($domainWithoutPort);
+    
+    if ($ip !== $domainWithoutPort) {
+        $steamConnectLink = "steam://connect/" . $ip . ":$port";
+        header("Location: " . $steamConnectLink);
+        exit;
+    } else {
+        header("Location: " . "steam://connect/" . $_GET['ip']);
+        exit;
+    }
+}
+?>
